7ef51a6fe4ea432e484b9cd4947bdad626476187
Alima <alimausmani21@navgurukul>
2024-10-04 14:37:11 +0530

    Added File

---
+++ src/StopWatch.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './StopWatch.css';
+
+const StopWatch = () => {
+  return (
+    <div>
+      <h1>Stop Watch</h1>
+    </div>
+
+  )
+}
+
+export default StopWatch
